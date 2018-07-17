8098f4a730fc900810ffb1891b9b5f10e828073d
Douglas Shimada <shimadadouglas@gmail.com>
2018-07-16 23:13:55 -0300

    add hello world

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,11 @@
+public class MyFirstJavaProgram{
+/* This is my first java program or not xD
+ * This will print hello world as the output
+ */
+    public static void main(String []args){
+        System.out.println("hello world");
+
+    
+    }
+
+}
